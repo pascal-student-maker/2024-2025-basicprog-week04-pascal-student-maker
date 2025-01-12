efc46793e9eec7acb4d65f1c36879af0f8e0ffa6
pascal-maker <pascal-musa@hotmail.com>
2025-01-12 23:24:59 +0100

    yes fixed

--- Ex18.py
+++ Ex18.py
@@ -1,13 +1,42 @@
-# brutowedde_arbeider = []
-# brutowedde_bediende = []
-# brutowedde_kader = []
-# input("Geef de functie op (A: Arbeider, B: Bediende, K: Kaderpersoneel): > ")
+amount_workers = []
+amount_clerks = []
+amount_managers = []
+total_salary = amount_salary = []
+def create_program_salary(type_of_employee:str,salary:int):
+    if type_of_employee == "W":
+        amount_workers.append(type_of_employee)
+        amount_salary.append(salary)
+    elif type_of_employee == "C": 
+        amount_clerks.append(type_of_employee) 
+        amount_salary.append(salary)
+    elif type_of_employee == "M":
+        amount_managers.append(type_of_employee) 
+        amount_salary.append(salary)
+    else:
+        print("Invalid category. Please Enter W,M,C")  
+        
+number_of_employees = int(input("Enter the amount of employees:"))
 
-a = input("Enter the number of employees:")
-salary_workers = []
-salary_clerks = []
-salary_managers = []
-a= print(input("Specify the type of employee (W: Worker, C: Clerk, M: Manager)")) 
-b = print(input("Enter the salary:>"))
-print(f" Specify the type of employee (W: Worker, C:Clerk, M: Manager: {a} Enter the salary: {b}")
+for _ in range(number_of_employees):
+    type_of_employee = input("Specify the type of employee:> ")  
+    salary = int(input("Enter the salary:>")) 
+    create_program_salary(type_of_employee,salary)  
+
+total_amount_employees = len(amount_clerks)  + len(amount_managers)  + len(amount_workers)  
+print(f" Amount of the workers {len(amount_workers)}")
+print(f" Amount of the clerks {len(amount_clerks)}")
+print(f" Amount of the managers{len(amount_managers)}")
+print(f" Total amount of employees: {total_amount_employees}")
+print(f" Total amount of salary: {sum(amount_salary)}")
+
+
+
+
+      
+        
+    
+        
+        
+    
+
 
