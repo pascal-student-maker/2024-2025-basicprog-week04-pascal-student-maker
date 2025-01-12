34ab2d5aa44715690d18ce34ff830989a7e4d579
pascal-maker <pascal-musa@hotmail.com>
2025-01-12 20:58:48 +0100

    exercise 16c updated

--- exercise16c.py
+++ exercise16c.py
@@ -1,16 +1,11 @@
-def give_even_positions(list1):
-    even_positions = []
-    odd_positions = []
-    for i in range(len(list1)):
-        if i % 2 == 0:
-            even_positions.append(list1[i])
-        else:
-            odd_positions.append(list1[i])
-    return even_positions
-
-list1 = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
-even_elements = give_even_positions(list1)
-
-print(f"Original list: {list1}")
-print(f"The elements in the even positions are: {even_elements}")        
+def give_even_positions(list1:list) -> list:
+    new_list = []
+    for element in list1[::2]:
+        print(element)
+        new_list.append(element)
         
+    return new_list 
+
+original_list = [ 0, 10, 20, 30, 40, 50, 60, 70, 80, 90] 
+print(f" Original list: {original_list}")
+print(f" The elements in the even positions are: {give_even_positions(original_list)}")  
