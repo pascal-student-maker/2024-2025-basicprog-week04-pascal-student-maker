7bff859c981cc669766a9fb3e7a46de603c97cd8
pascal-maker <pascal-musa@hotmail.com>
2025-01-12 16:35:40 +0100

    exercise7

--- Ex07.py
+++ Ex07.py
@@ -1,3 +1,19 @@
-# collection1 = [12, 45, -9, -15]
-# collection2 = [12.23, 45.1, 9.478, 15.125, -3.14]
-# collection3 = ["Joerie", "Marie", "Henk", "Stijn"]
+def get_info_list(name:str,collection:list) -> str:
+    result = f" {name}\n"
+    for i  in range(len(collection)):
+        result += f" {collection[i]} is  on position {i}\n"
+        return result
+    
+        
+        
+        
+collection1 = [12, 45, -9, -15]
+collection2 = [12.23, 45.1, 9.478, 15.125, -3.14]
+collection3 = ["Joerie", "Marie", "Henk", "Stijn"]
+
+# Call the function for multiple lists
+print(get_info_list("Collection Non-decimal numbers", collection1))
+print(get_info_list("Collection Decimal numbers", collection2))
+print(get_info_list("Collection Strings", collection3))
+        
+        
